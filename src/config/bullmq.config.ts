d85c91ef5aaddb4edf06ac0b120210a847c0dc00
nkhy-3112-dev <hynk@suga.com.vn>
2024-12-17 14:08:56 +0700

    update transaction

--- src/config/bullmq.config.ts
+++ src/config/bullmq.config.ts
@@ -2,5 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('bullmq', () => ({
   db: process.env.BULLMQ_REDIS_PORT,
-  transactionQueue: process.env.BULLMQ_TRANSACTION_QUEUE,
 }));
