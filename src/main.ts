8e2c60cedcdeae53cbd915e5e2abb15e277edf93
nkhy-3112-dev <hynk@suga.com.vn>
2024-12-24 20:59:54 +0700

    update: docker compose file

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ async function bootstrap() {
   initializeTransactionalContext();
   const app = await initApplication();
 
+  const configService = app.get(ConfigService);
+
+  const origins = configService.get<string>('CORS_ORIGINS', '');
+  console.log('origin', origins);
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://10.66.53.204:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: origins.split(','),
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   const port = app.get(ConfigService).get<number>('app.port') || 3000;
