658acff985b97e4bd9f312bb78186f0677669d15
hnminhhuy <dev.hnmhuy@gmail.com>
2025-01-01 14:37:42 +0700

    chore: update env

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   name: process.env.APP_NAME,
-  port: process.env.APP_PORT,
+  port: process.env.PORT || 3000,
   env: process.env.NODE_ENV,
   debug: process.env.APP_DEBUG === 'true',
   version: process.env.APP_VERSION,
