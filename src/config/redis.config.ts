b17d408091946e00f945b6bb072f25f4e57b5901
hnminhhuy <dev.hnmhuy@gmail.com>
2025-01-03 22:17:46 +0700

    chore: update redis config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -6,6 +6,11 @@ export default registerAs('redis', () => ({
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
   db: parseInt(process.env.REDIS_DB) || 0,
-  // tls: process.env.REDIS_TLS === 'true' ? {} : undefined, // Optional TLS configuration
+  tls:
+    process.env.REDIS_TLS === 'true'
+      ? {
+          rejectUnauthorized: true,
+        }
+      : undefined, // Optional TLS configuration
   logging: process.env.REDIS_LOGGING === 'true', // Enable logging if needed
 }));
